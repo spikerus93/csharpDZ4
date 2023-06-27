35ed54d9b6d50dd41d060606e7fc1da1067149e4
master <spikerus93@rambler.ru>
2023-06-27 18:15:21 +0300

    Домашнее задания по 4-ому Семинару

---
+++ task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B. НЕ ИСПОЛЬЗОВАТЬ MATH.POW()
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А:  ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:  ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int A_B=1;
+  for(int i=0; i<B; i++) 
+  {
+   A_B*=A;
+  }
+  Console.WriteLine("{0} ^ {1} = {2}", A, B, A_B);
---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+//  и выводит их на экран.
+
+int[] GetRandomArray()
+{    int[] Array = new int[8];
+    for(int i=0; i<Array.Length;i++)
+    {
+        Array[i] = Random.Shared.Next(-999, 1000);
+    }
+    return Array;
+}
+
+int[] MyArray = GetRandomArray();
+Console.WriteLine($"[{String.Join(", " , MyArray)}]");
